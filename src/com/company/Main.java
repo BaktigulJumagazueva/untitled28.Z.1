d8fd0001748e73ba24d24f4862ae4ad8c38d02da
13 <baktyguljumagazyeva@gmail.com>
2022-09-20 17:41:19 +0600

    DZ28.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,21 @@
+package com.company;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(5,23,6,0,12,123,6,5,1,99,3,4,1,2);
+         List<Integer> list1= Collections.singletonList(list.stream().mapToInt((x)->Integer.parseInt(x.toString())).sum());
+        System.out.println("srednee znachenie");
+        list.stream().mapToInt(i->i).average().ifPresent(System.out::println);
+        System.out.println("Summa");
+        OptionalDouble list3= list.stream().mapToInt((x)->Integer.parseInt((String.valueOf(x)))).average();
+        System.out.println(list1);
+        System.out.println(list.stream().collect(Collectors.groupingBy(Function.identity())));
+        System.out.println("kolichestvo znachenii");
+        System.out.println(list.size());
+
     }
 }
